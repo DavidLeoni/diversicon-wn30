80865776de7bbc70b8b321faa6bdd1411a77375b
David Leoni <davidleonigit-github@yahoo.com>
2016-06-27 13:57:16 +0200

    added both main and test data

---
+++ src/main/java/it/unitn/disi/diversicon/data/wn30/DivWn30.java
@@ -0,0 +1,8 @@
+package it.unitn.disi.diversicon.data.wn30;
+
+public class DivWn30 {
+    public static final String WORDNET_DIV_H2_DB_RESOURCE_URI = "classpath:/it/unitn/disi/diversicon/data/wn30/div-wn30.h2.db.xz";
+    public static final String WORDNET_DIV_SQL_RESOURCE_URI = "classpath:/it/unitn/disi/diversicon/data/wn30/div-wn30.sql.xz";
+    public static final String WORDNET_UBY_XML_RESOURCE_URI = "classpath:/it/unitn/disi/diversicon/data/wn30/uby-wn30.xml.xz";
+
+}
---
+++ src/test/java/it/unitn/disi/diversicon/data/wn30/test/DivWn30Test.java
@@ -0,0 +1,5 @@
+package it.unitn.disi.diversicon.data.wn30.test;
+
+public class DivWn30Test {
+    public static final String WORDNET_DIV_DB_RESOURCE_URI = "classpath:/it/unitn/disi/diversicon/data/wn30/test/uby-wn30.sql.xz";    
+}
