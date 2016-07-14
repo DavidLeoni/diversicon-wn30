ff77f1f435702b094c54932807b49e053ac66518
David Leoni <davidleonigit-github@yahoo.com>
2016-07-14 14:44:28 +0200

    added version

--- src/main/java/it/unitn/disi/diversicon/data/wn30/DivWn30.java
+++ src/main/java/it/unitn/disi/diversicon/data/wn30/DivWn30.java
@@ -1,8 +1,18 @@
 package it.unitn.disi.diversicon.data.wn30;
 
 public class DivWn30 {
+        
+    public static final String ID = "it.unitn.disi.diversicon.data.wn30.div-wn30";
+    
+    /**
+     * This version should coincide with {@link it.unitn.disi.diversicon.DbInfo#version} inside the db.
+     */
+    public static final String VERSION = "0.1.0-SNAPSHOT";
     public static final String WORDNET_DIV_H2_DB_RESOURCE_URI = "classpath:/it/unitn/disi/diversicon/data/wn30/div-wn30.h2.db.xz";
     public static final String WORDNET_DIV_SQL_RESOURCE_URI = "classpath:/it/unitn/disi/diversicon/data/wn30/div-wn30.sql.xz";
     public static final String WORDNET_UBY_XML_RESOURCE_URI = "classpath:/it/unitn/disi/diversicon/data/wn30/uby-wn30.xml.xz";
 
 }
+
+
+
